90199e43b4015819d6eb022863f2e20771b11536
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2026-04-23 22:23:01 -0400

    back half of the mt pyre split

--- src/lottery_corner.c
+++ src/lottery_corner.c
@@ -13,7 +13,8 @@ static EWRAM_DATA u16 sOtIdDigit = 0;
 
 static const u16 sLotteryPrizes[] =
 {
-    ITEM_PP_UP,
+    ITEM_NUGGET,
+    // ITEM_PP_UP,
     ITEM_EXP_SHARE,
     ITEM_MAX_REVIVE,
     ITEM_MASTER_BALL,
